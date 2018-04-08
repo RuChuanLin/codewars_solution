3c5251c4c9a0da0b8986dc08bf7aef3c25634fab
Hazel Wu <tinazx056@gmail.com>
2018-04-08 12:44:37 +0800

    Resolve 8kyu: A needle in the Haystack

---
+++ 8kyu/A Needle in the Haystack.js
@@ -0,0 +1,5 @@
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle");
+}
+
+findNeedle([3,123124234, undefined, 'needle', 'world', 'hay', 2, 3, true, false]);
